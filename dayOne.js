3d49aeca1f57f21eeb8c139518201d09f405d5e4
Anil K. Panta <anilpanta2053@gmail.com>
2021-12-11 22:50:00 +0545

    day One

---
+++ dayOne.js
@@ -0,0 +1,6 @@
+function randomNumberGeneratorInRange(rangeStart, rangeEnd) {
+  return Math.floor(Math.random() * (rangeEnd - rangeStart + 1)) + rangeStart;
+}
+
+console.log(`My random number: ${randomNumberGeneratorInRange(1000, 5000)}`);
+console.log(`My random number: ${randomNumberGeneratorInRange(100, 200)}`);
